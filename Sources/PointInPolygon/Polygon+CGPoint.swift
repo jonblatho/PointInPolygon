ce75fe4c2ef1a4f84538a8300c15f73ab662542e
Jonathan Thornton <github@jonblatho.com>
2020-10-12 17:27:43 -0500

    add convenience init for CGPoints

--- Sources/PointInPolygon/Polygon+CGPoint.swift
+++ Sources/PointInPolygon/Polygon+CGPoint.swift
@@ -1,8 +1,16 @@
-//
-//  File.swift
-//  
-//
-//  Created by Jonathan Thornton on 10/12/20.
-//
-
 import Foundation
+
+extension Polygon {
+    /**
+     Initializes a Polygon from an array of `CGPoint` objects.
+     
+     - parameter cgPoints: An array of `CGPoint` objects containing the polygon's points.
+     */
+    public convenience init(cgPoints: [CGPoint]) {
+        let points: [Point] = cgPoints.map { (cgPoint) -> Point in
+            Point(x: Double(cgPoint.x), y: Double(cgPoint.y))
+        }
+        
+        self.init(points: points)
+    }
+}
